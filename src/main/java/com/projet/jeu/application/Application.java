d0a38f51557c72b59b043a531db76aa359baf5c0
junker52 <r.molina91@gmail.com>
2017-09-16 12:49:18 +0200

    log4j ajouté: configuration + precise a faire.

--- src/main/java/com/projet/jeu/application/Application.java
+++ src/main/java/com/projet/jeu/application/Application.java
@@ -1,10 +1,23 @@
 package com.projet.jeu.application;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 import com.projet.jeu.menu.Menu;
 
 public class Application {
 
+	public static final Logger log = Logger.getLogger(Application.class);
+
 	public static void main(String[] args) {
+
+		// Configuracion base del logj4 obtenida del archivo log4.properties
+		//VER COMO CONFIGURAR LOG4J PARA QUE NO SE MUESTRE EN CONSOLA
+		BasicConfigurator.configure();
+
+		// Logeando mensajes
+		log.info("Message de test type info");
+		log.fatal("Coucou, epic fail");
 		Menu menu = new Menu();
 		short rep = menu.debutMenu();
 		short subrep = menu.submenu();
